720c2b8ed7e9507a380274a43db9794921ff9c22
Linda <linda.dogan@outlook.fr>
2020-07-15 17:09:37 +0200

    DockerFile problem

--- TP1/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-var server = app.listen(3000, function() {
-  var port = server.address().port;
-  console.log("Server started at http://localhost:%s", port)
-});
-
--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var calculator = require('./calculator');
- 
 var a=10, b=5;
  
 console.log("Addition : "+calculator.add(a,b));
 console.log("Subtraction : "+calculator.subtract(a,b));
 console.log("Multiplication : "+calculator.multiply(a,b));
 console.log("Division : "+calculator.divison(a,b));
-
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
-});
